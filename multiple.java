d70921deba8dce95b51a52901abb759aa9618b92
khg-it <120780238+khg-it@users.noreply.github.com>
2023-02-09 22:57:19 -0800

    Add files via upload

---
+++ multiple.java
@@ -0,0 +1,14 @@
+public class multiple {
+    public static void main(String args[])
+    {
+        int n=6;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(5*j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
